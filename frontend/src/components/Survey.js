1c60c3a1808001f89f8bc02e3ee134ed5ef6cc62
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-10-27 21:07:05 -0400

    fixed form to load text after setstate

--- frontend/src/components/Survey.js
+++ frontend/src/components/Survey.js
@@ -1,151 +1,170 @@
-import React from 'react';
-import { Slider, Icon } from "react-native-elements";
-import {
-  SafeAreaView,
-StyleSheet,
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  Animated,
-} from "react-native";
-import { Value } from 'react-native-reanimated';
+
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+import Slider from "@react-native-community/slider";
 
 
-const Survey = () => {
+class SliderExample extends React.Component {
+  static defaultProps = {
+    value: 1,
+    text: "",
+  };
 
+  state = {
+    value: this.props.value,
+    text: this.props.text
+  };
+
+
+
+   handleChange = (value) =>  this.setState({value: value}, () => {this.text()});
+
+  text = () => {
+switch (this.state.value) {
+  case 1:
+    text = "Very Tired";
+    break;
+  case 2:
+    text = "Tired";
+    break;
+  case 3:
+    text = "Not Tired but Not Refreshed";
+    break;
+  case 4:
+    text = "Refreshed";
+    break;
+  case 5:
+    text = "Very Refreshed";
+    break;  
+}
+return this.setState({text:text})
+  }
+
+
+  render() {
+    
     return (
-      <SafeAreaView style={styles.container}>
-        <View
-          style={{ flex: 1, alignItems: "stretch", justifyContent: "center" }}
-        >
-          <Slider
-            style={{
-              color: "white",
-              width: 200,
-              maximumValue: 5,
-              minimumValue: 1,
-            }}
-
-            // value={this.state.value}
-
-            // onValueChange={(value) => this.setState({ value })}
-          />
-
-          <Text
-            style={{ color: "white"}}
-          >
-            6
-          </Text>
-        </View>
-
-        <View
-          style={{ flex: 1, alignItems: "stretch", justifyContent: "center" }}
-        >
-          <Slider
-            // value={this.state.value}
-            // onValueChange={(value) => this.setState({ value })}
-            thumbStyle={{
-              height: 40,
-              width: 100,
-              backgroundColor: "transparent",
-            }}
-            thumbProps={{
-              Component: Animated.Image,
-              source: {
-                uri:
-                  "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
-              },
-            }}
-          />
-          <Text>Value: 5</Text>
-        </View>
-        <View
-          style={{ flex: 1, alignItems: "stretch", justifyContent: "center" }}
-        >
-          <Slider
-            // value={value}
-            // onValueChange={setValue}
-            maximumValue={50}
-            minimumValue={20}
-            step={1}
-            trackStyle={{ height: 10, backgroundColor: "transparent" }}
-            thumbStyle={{
-              height: 20,
-              width: 200,
-              backgroundColor: "transparent",
-            }}
-            thumbProps={{
-              children: (
-                <Icon
-                  name="heartbeat"
-                  type="font-awesome"
-                  size={20}
-                  reverse
-                  containerStyle={{ bottom: 20, right: 20 }}
-                  color="#f50"
-                />
-              ),
-            }}
-          />
-          <Text>Value: 40</Text>
-        </View>
-      </SafeAreaView>
+      <View>
+        <Text style={styles.text}>{this.state.value && this.state.value}</Text>
+        <Slider
+          style={styles.slider}
+          {...this.props}
+          onValueChange={this.handleChange}          
+          style={{ width: 200, height: 40 }}
+          minimumValue={1}
+          maximumValue={5}
+          minimumTrackTintColor="red"
+          step="1"
+          maximumTrackTintColor="blue"
+        />
+       <Text>{this.state.text}</Text>
+      </View>
     );
-};
+   
+  }
+ 
+
+}
+
+
+class Survey extends React.Component {
+  state = {
+    slideStartingValue: 0,
+    slideStartingCount: 0,
+  };
+
+  render() {
+     
+    return (
+
+      <View>
+        <Text>{displayName}</Text>
+        <Text>{this.state.text}</Text>
+        <SliderExample
+          {...this.props}
+          onSlidingStart={(value) =>
+            this.setState({
+              slideStartingValue: value,
+              slideStartingCount: this.state.slideStartingCount + 1,
+            })
+          }
+        />
+        <Text>
+          Starts: {this.state.slideStartingCount} Value:{" "}
+          {this.state.slideStartingValue}
+        </Text>
+      </View>
+    );
+   
+  }
+
+
+}
+
 export default Survey;
 
 const styles = StyleSheet.create({
-  container: {
-    height: "100%",
-    backgroundColor: "black",
-    color: "white",
-    alignItems: "center",
-    justifyContent: "center",
+  slider: {
+    width: 300,
+    opacity: 1,
+    height: 50,
+    marginTop: 50,
   },
   text: {
-    color: "white",
-    fontSize: 40,
-    marginBottom: 30,
-  },
-  img: {
-    width: "100%",
-    height: 150,
-    borderRadius: 5,
-  },
-  text2: {
-    color: "white",
-    margin: "12%",
+    fontSize: 14,
     textAlign: "center",
-    justifyContent: "flex-start",
-
-    fontSize: 15,
-  },
-  middle: {
-    width: 200,
-  },
-  btn: {
-    borderColor: "#167bff",
-    borderWidth: 1,
-    width: 100,
-    height: 45,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "black",
-    borderRadius: 10,
-    shadowColor: "white",
-    shadowOffset: { width: 1, height: 1 },
-    shadowOpacity: 1,
-    shadowRadius: 3,
-    color: "white",
-    margin: 5,
-  },
-  text3: {
-    color: "white",
-    fontSize: 20,
-    textAlign: "center",
-  },
-  buttons: {
-    flexDirection: "row",
+    fontWeight: "500",
+    margin: 10,
   },
 });
+
+export const title = "<Slider>";
+export const displayName = "SliderExample";
+export const description = "Slider input for numeric values";
+
+
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     height: "100%",
+//     backgroundColor: "black",
+//     color: "white",
+//     alignItems: "center",
+//     justifyContent: "flex-start",
+//   },
+//   container2: {
+//     flexDirection: "column",
+//   },
+//   text: {
+//     flex: 0.5,
+//     color: "white",
+//     fontSize: 30,
+//   },
+
+//   text2: {
+//     flex: 0.5,
+//     color: "white",
+//     textAlign: "center",
+//     justifyContent: "center",
+//     fontSize: 20,
+//   },
+//   image: {
+//     width: 180,
+//     height: 200,
+//     borderRadius: 5,
+//   },
+//   containerofimage: {
+//     flex: 2,
+//     width: 200,
+//     flexDirection: "column",
+//     alignItems: "center",
+//     justifyContent: "flex-start",
+//     marginBottom: "10%",
+//   },
+//   bottombuttons: {
+//     flex: 2,
+//     alignItems: "center",
+//     justifyContent: "flex-start",
+//   },
+// });
