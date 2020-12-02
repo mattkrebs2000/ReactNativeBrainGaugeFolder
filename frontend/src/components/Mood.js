80f52eac2960f0f6b65c932f124762feae7d6e46
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-12-02 15:40:48 -0500

    Victory Charts Installed

--- frontend/src/components/Mood.js
+++ frontend/src/components/Mood.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { useNavigation } from "@react-navigation/native";
 import {
   SafeAreaView,
@@ -8,8 +9,128 @@ import {
   Image,
   TouchableOpacity,
 } from "react-native";
+import { VictoryChart, VictoryScatter, VictoryTheme } from "victory-native";
 
-const Home = () => {
+const Mood = () => {
+
+// const [options, setOptions] = useState({
+//   grid: {
+//     padding: {
+//       left: 10,
+//     },
+//   },
+//   chart: {
+//     height: 350,
+//     type: "line",
+//     toolbar: {
+//       show: true,
+
+//       offsetX: 0,
+//       offsetY: 0,
+//       tools: {
+//         download: false,
+
+//         selection: true,
+//         zoom: true,
+//         zoomin: true,
+//         zoomout: true,
+//         pan: true,
+//         reset: true | '<img src="/static/icons/reset.png" width="20">',
+//         customIcons: [],
+//       },
+//       autoSelected: "zoom",
+//     },
+//   },
+//   fill: {
+//     type: "solid",
+//   },
+//   markers: {
+//     size: [6, 0],
+//   },
+//   tooltip: {
+//     shared: false,
+//     intersect: true,
+//     fillSeriesColor: true,
+//     theme: true,
+//     style: {
+//       fontSize: "20px",
+//       colors: "#000",
+//     },
+//   },
+//   legend: {
+//     show: true,
+//     labels: {
+//       colors: "white",
+//       useSeriesColors: true,
+//     },
+//     fontSize: "20",
+//   },
+//   xaxis: {
+//     axisTicks: {
+//       show: true,
+//       height: 5,
+//       width: "100%",
+//       offsetX: 0,
+//       offsetY: 0,
+//     },
+//     labels: {
+//       offsetY: -3,
+//       style: {
+//         fontSize: "15",
+//         colors: "white",
+//         padding: "50",
+//       },
+//     },
+
+//     type: "numeric",
+//     min: 0,
+//     max: 5,
+//     tickAmount: 5,
+//     title: {
+//       text: "1.0 = Very Tired; 5.0 = Wide Awake",
+//       margin: 100,
+//       floating: false,
+//       style: {
+//         color: "#FFF",
+//         fontSize: window.innerWidth > 540 ? 20 : 15,
+//         cssClass: "animal",
+//       },
+//     },
+//   },
+//   yaxis: {
+//     labels: {
+//       style: {
+//         fontSize: "15",
+//         colors: "white",
+//       },
+//     },
+//     title: {
+//       text: "Reaction Time",
+//       style: {
+//         color: "white",
+//         fontSize: "20",
+//         cssClass: "animal",
+//       },
+//     },
+
+//     type: "numeric",
+//     min: 0,
+
+//     tickAmount: window.innerWidth > 640 ? 5 : 4,
+//   },
+// }); 
+
+// const [series, setSeries] = useState([
+//   {
+//     name: "Reaction Time",
+//     type: "scatter",
+//     //2.14, 2.15, 3.61, 4.93, 2.4, 2.7, 4.2, 5.4, 6.1, 8.3
+//     data: [],
+//   },
+// ]);
+
+
+
 //   const navigation = useNavigation();
 
   console.log("Mood");
@@ -17,6 +138,24 @@ const Home = () => {
     <SafeAreaView style={styles.container2}>
       <View style={styles.container}>
         <Text style={styles.text}>Brain Gauge</Text>
+
+<VictoryChart
+  theme={VictoryTheme.material}
+  domain={{ x: [0, 5], y: [0, 7] }}
+>
+  <VictoryScatter
+    style={{ data: { fill: "#c43a31" } }}
+    size={7}
+    data={[
+      { x: 1, y: 2 },
+      { x: 2, y: 3 },
+      { x: 3, y: 5 },
+      { x: 4, y: 4 },
+      { x: 5, y: 7 }
+    ]}
+  />
+  </VictoryChart>
+       
 
         <View style={styles.middle}>
           <Image
@@ -69,7 +208,7 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Mood;
 
 const styles = StyleSheet.create({
   container: {
